0a20538b5ff6976371b75b521a22c0d754685968
jordanbemmels <55410762+jordanbemmels@users.noreply.github.com>
2021-10-22 16:37:23 -0400

    Update splitVCFbyScaffold_git.R

--- PopSizeABC/splitVCFbyScaffold_git.R
+++ PopSizeABC/splitVCFbyScaffold_git.R
@@ -1,1 +1,33 @@
+# code is to split a single whole-genome vcf file for a kiwi population into separate vcf files for each scaffold;
+# note that a .vcf and a .vcf.gz are BOTH required (also the tabix .tbi file for the .gz);
 
+args <- commandArgs(trailingOnly = T);
+
+infile <- args[1];
+outdir <- args[2];
+
+#print(infile)
+#print(outdir)
+
+# get a list of all the scaffolds - but remove any with <2 SNPs;
+
+vcf <- read.table(infile, sep = "\t", header = F, comment.char = "#", stringsAsFactors = F);
+
+scaffolds <- unique(vcf$V1);
+
+n_snps_per_scaffolds <- data.frame(scaffold = scaffolds, n_snps = NA);
+for (i in 1:length(scaffolds)) {
+	n_snps_per_scaffolds$n_snps[i] <- sum(vcf$V1 == scaffolds[i]);
+}
+
+scaffolds2 <- scaffolds[n_snps_per_scaffolds$n_snps >= 2];
+
+# split the vcf into each of scaffolds2 using bcftools;
+
+for (i in 1:length(scaffolds2)) {
+
+	print(paste0("working on scaffold ", i, " of ", length(scaffolds2)));
+	splitCommand <- paste0("bcftools view -r ", scaffolds2[i], " ", infile, ".gz > ", outdir, "/", outdir, "_", scaffolds2[i], ".vcf");
+	system(splitCommand);
+
+}
